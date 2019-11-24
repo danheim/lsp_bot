0002e895b16d8cfa8e996659d6a1e4558bd606b4
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-11-25 00:03:31 +0200

    1

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 const config = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
+    "username": "samp",
+    "password": "l1s2p3",
+    "database": "chromakeeey",
     "host": "51.38.127.130",
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "test": {
-    "username": process.env.DB_NAME,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
+    "username": "samp",
+    "password": "l1s2p3",
+    "database": "chromakeeey",
     "host": "51.38.127.130",
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "production": {
-    "username": process.env.DB_NAME,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
+    "username": "samp",
+    "password": "l1s2p3",
+    "database": "chromakeeey",
     "host": "51.38.127.130",
     "dialect": "mysql",
     "operatorsAliases": false
